248da118c6d25c8b790c03ef5efe040913023907
samdossant <samuelds2004@outlook.com>
2024-06-01 16:18:11 +0200

    Fixed test, and documented

--- SocialShuffle/tests/Browser/CreateTeamTest.php
+++ SocialShuffle/tests/Browser/CreateTeamTest.php
@@ -12,57 +12,58 @@ class CreateTeamTest extends DuskTestCase
     use DatabaseMigrations;
 
     /**
-     * Test that creating a full test works
+     * Test that creating a full team works
      */
-    public function testCreateTeam(): void
-    {
-        // Arrange
-        $user = User::factory()->create([
-            'username' => 'jDoe',
-            'email' => 'johndoe@gmail.com',
-            'password' => bcrypt('0000'),
-            'admin' => false,
-        ]);
+public function testCreateTeam(): void
+{
+    // Arrange
+    $user = User::factory()->create([
+        'username' => 'jDoe',
+        'email' => 'johndoe@gmail.com',
+        'password' => bcrypt('0000'),
+        'admin' => false,
+    ]);
 
-        // Act
-        $this->browse(function (Browser $browser) use ($user) {
-            $browser->visit('/')
+    // Act
+    $this->browse(function (Browser $browser) use ($user) {
+        $browser->visit('/')
 
-                    // Login
-                    ->clickLink('Se connecter')
-                    ->type('email', $user->username)
-                    ->type('password', '0000')
-                    ->click('@login-button')
+                // Login
+                ->clickLink('Se connecter')
+                ->type('email', $user->username)
+                ->type('password', '0000')
+                ->click('@login-button')
 
-                    // Team name
-                    ->clickLink('+ Nouvelle équipe')
-                    ->type('name', 'duskTeam')
-                    ->click('@next-button')
+                // Team name
+                ->clickLink('+ Nouvelle équipe')
+                ->type('name', 'duskTeam')
+                ->click('@next-button')
 
-                    // Add members
-                    ->type('firstname', 'm1')
-                    ->type('lastname', 'm1')
-                    ->type('email', 'm1@a.a')
-                    ->type('phone_number', '9999999999')
-                    ->click('@add-member')
-                    
-                    ->type('firstname', 'm2')
-                    ->type('lastname', 'm2')
-                    ->type('email', 'm2@a.a')
-                    ->type('phone_number', '9999999999')
-                    ->click('@add-member')
-                    
-                    ->click('@next')
-                    
-                    ->type('nbMemberPerGroup', 1)
-                    ->type('nbActivities', 1)
+                // Add members
+                ->type('firstname', 'm1')
+                ->type('lastname', 'm1')
+                ->type('email', 'm1@a.a')
+                ->type('phone_number', '9999999999')
+                ->click('@add-member')
+                
+                ->type('firstname', 'm2')
+                ->type('lastname', 'm2')
+                ->type('email', 'm2@a.a')
+                ->type('phone_number', '9999999999')
+                ->click('@add-member')
+                
+                ->click('@next')
+                
+                // Group generation data
+                ->type('nbMemberPerGroup', 1)
+                ->type('nbActivities', 1)
 
-                    ->click('@create-groups')
+                ->click('@create-groups')
 
-                    ->screenshot('finish')
+                ->screenshot('finish')
 
-                    // Assert
-                    ->assertPathIs('/team/1');
-        });
-    }
+                // Assert
+                ->assertPathIs('/team/1');
+    });
+}
 }
